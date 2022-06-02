8bcdaca1e810b3308ead20580da5f43554c79016
havana723 <71638185+havana723@users.noreply.github.com>
2022-06-03 05:27:54 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import React from "react";
+import Sparkle from "react-sparkle";
+import "./App.css";
+
+const imgs = [
+  "/img/137_0.png",
+  "/img/137_1.png",
+  "/img/137_2.png",
+  "/img/137_3.png",
+  "/img/137_4.png",
+  "/img/137_5.png",
+  "/img/137_6.png",
+];
+
+const Background = styled.div`
+  background-color: black;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  animation: hue 8s infinite linear;
+
+  @keyframes hue {
+    0% {
+      background-color: black;
+    }
+    25% {
+      background-color: #10132f;
+    }
+    75% {
+      background-color: #1a0828;
+    }
+    100% {
+      background-color: black;
+    }
+  }
+`;
+
+const Title = styled.div`
+  color: white;
+  text-align: center;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 6vh;
+`;
+
+const Text = styled.div`
+  color: white;
+  text-align: center;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 6vh;
+`;
+
+const ImgDiv = styled.div`
+  position: relative;
+  height: 60vh;
+  width: min(60vh, 100vw);
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+  transition: all 0.4s ease-in-out;
+
+  &:hover {
+    transform: scale(1.4);
+  }
+`;
+
+const Img = styled.img`
+  height: min(40vh, 100vw);
+  animation: floating 4s ease-in-out infinite;
+
+  @keyframes floating {
+    0% {
+      transform: translate(0, 0vh);
+    }
+    50% {
+      transform: translate(0, 3vh);
+    }
+    100% {
+      transform: translate(0, -0vh);
+    }
+  }
+`;
 
 function App() {
+  const mx_cnt = 6;
+  const cnt = 2;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Background>
+        <Sparkle
+          count={50}
+          fadeOutSpeed={10}
+          flickerSpeed="slowest"
+          overflowPx={0}
+        />
+        <div style={{ height: "10vh" }} />
+        <Title>동사몇?</Title>
+        <ImgDiv>
+          {cnt <= mx_cnt ? <Img src={imgs[cnt]} /> : <Img src={imgs[mx_cnt]} />}
+        </ImgDiv>
+        <Text>{cnt} 명</Text>
+      </Background>
+    </>
   );
 }
 
